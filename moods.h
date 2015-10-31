2a3530ed14075532a41c00d2b95a89ff91d2f669
Tristan Lucas <tristan.lucas.de@gmail.com>
2015-10-31 08:44:29 +0100

    Cleaned up warnings and insane funcs

--- moods.h
+++ moods.h
@@ -11,6 +11,9 @@ typedef struct {
 
 extern HV mood_arr[N_COLS][N_ROWS];
 
+void fill_mood(uint16_t starting_pos, float starting_h, float starting_v,
+	uint8_t func);
+
 void mood1(void);
 void mood2(void);
 void mood3(void);
