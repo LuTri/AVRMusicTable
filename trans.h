1f92eaa4e9c0180d2ebc681ec94127e783b3935c
Tristan Lucas <tristan.lucas.de@gmail.com>
2015-10-31 09:21:18 +0100

    Implemented "Distance" functions

--- trans.h
+++ trans.h
@@ -4,4 +4,7 @@
 uint8_t coord_to_snakish(uint8_t x, uint8_t y);
 void snakish_to_coord(uint8_t snake, uint8_t* x, uint8_t* y);
 
+float distance_coord(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2);
+float distance_snake(uint8_t pos1, uint16_t pos2);
+
 #endif
