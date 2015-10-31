dcb83b3170626ce80831b12448478d7e284b084c
Tristan Lucas <tristan.lucas.de@gmail.com>
2015-10-31 09:21:55 +0100

    Made "moods" actually use mood-functions

--- moods.c
+++ moods.c
@@ -1,19 +1,40 @@
 #include "moods.h"
+#include "modes.h"
+#include "color.h"
+#include "config.h"
+#include "trans.h"
 
-HV mood_arr[N_COLS][N_ROWS];
+extern cRGB leds[N_LEDS];
 
-void fill_mood(uint16_t starting_pos, float starting_h, float starting_v,
+uint16_t cycle = 0;
+
+void mood1(float h, float v, float distance, RGB* vals) {
+	fast_hsi(h, v, vals);
+}
+
+void mood2(float h, float v, float distance, RGB* vals) {
+}
+
+void mood3(float h, float v, float distance, RGB* vals) {
+}
+
+void mood4(float h, float v, float distance, RGB* vals) {
+}
+
+void (*funcs[])(float, float, float, RGB*) = {mood1, mood2, mood3, mood4};
+
+void fill_mood(uint8_t starting_pos, float starting_h, float starting_v,
 	uint8_t func) {
-}
+	uint8_t idx;
+	float distance;
+	RGB rgb;
 
-void mood1(void) {
-}
+	for (idx = 0; idx < N_LEDS; idx++) {
+		distance = distance_snake(idx, starting_pos);
+		(*funcs[func])(starting_h, starting_v, distance, &rgb);
 
-void mood2(void) {
-}
-
-void mood3(void) {
-}
-
-void mood4(void) {
+		leds[idx].r = rgb.r;
+		leds[idx].g = rgb.g;
+		leds[idx].b = rgb.b;
+	}
 }
