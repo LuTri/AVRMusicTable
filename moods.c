fff29dfd558f6f9456a5e6c5e4bbb6ee7539c1a2
Tristan Lucas <tristan.lucas.de@gmail.com>
2015-11-04 19:53:35 +0100

    Cleaned up moods.*, added return value to "fill_moods"

--- moods.c
+++ moods.c
@@ -6,38 +6,31 @@
 
 extern cRGB leds[N_PACKS];
 
-uint16_t cycle = 0;
+typedef struct {
+	float hue;
+	float value;
+	float distance;
+} HVd;
 
-void mood1(float h, float v, float distance, RGB* vals) {
-	fast_hsi(h, v, vals);
+void mood1(uint8_t idx, HVd* hvd, RGB* vals) {
 }
 
-void mood2(float h, float v, float distance, RGB* vals) {
-	fast_hsi(h, v, vals);
+void mood2(uint8_t idx, HVd* hvd, RGB* vals) {
 }
 
-void mood3(float h, float v, float distance, RGB* vals) {
-	fast_hsi(h, v, vals);
+void mood3(uint8_t idx, HVd* hvd, RGB* vals) {
 }
 
-void mood4(float h, float v, float distance, RGB* vals) {
-	fast_hsi(h, v, vals);
+void mood4(uint8_t idx, HVd* hvd, RGB* vals) {
 }
 
-void (*funcs[])(float, float, float, RGB*) = {mood1, mood2, mood3, mood4};
+void (*funcs[])(uint8_t, HVd*, RGB*) = {mood1, mood2, mood3, mood4};
 
-void fill_mood(uint8_t starting_pos, float starting_h, float starting_v,
+uint8_t fill_mood(uint8_t starting_pos, float starting_h, float starting_v,
 	uint8_t func) {
 	uint8_t idx;
-	float distance;
 	RGB rgb;
+	HVd hvd;
 
-	for (idx = 0; idx < N_PACKS; idx++) {
-		distance = distance_snake(idx, starting_pos);
-		(*funcs[func])(starting_h, starting_v, distance, &rgb);
-
-		leds[idx].r = rgb.r;
-		leds[idx].g = rgb.g;
-		leds[idx].b = rgb.b;
-	}
+	return 0;
 }
