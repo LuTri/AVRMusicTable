8eda9be74688f3aff6dd277f2596ed187b5b31f1
Tristan Lucas <tristan.lucas.de@gmail.com>
2015-11-04 19:48:41 +0100

    Let fast_hsi work with percent

--- color.c
+++ color.c
@@ -1,5 +1,9 @@
 #include "color.h"
 
+#define RANGE 100.0
+#define FIRST_THIRD RANGE/3
+#define SECOND_THIRD RANGE - FIRST_THIRD
+
 inline float cycle(float value, float step) {
 	while (value < 0) value += step;
 	while (value >= step) value -= step;
@@ -11,22 +15,21 @@ void fast_hsi(float value, float intensity, RGB* rgb) {
 	float r_p = 0, g_p = 0, b_p = 0;
 	float tmp_val;
 		
-	value = cycle(value,900.0);
-	tmp_val = 100.0 - (
-		cycle(value, 300.0) / 300.0 * 100.0);
+	value = cycle(value,RANGE);
+	tmp_val = RANGE - cycle(value, RANGE);
 
-	if (value >= 0 && value < 300.0) {
+	if (value >= 0 && value < FIRST_THIRD) {
 		r_p = tmp_val * intensity;
-		g_p = (100 - tmp_val) * intensity;
-	} else if (value >= 300.0 && value < 600) {
+		g_p = (RANGE - tmp_val) * intensity;
+	} else if (value >= FIRST_THIRD && value < SECOND_THIRD) {
 		g_p = tmp_val * intensity;
-		b_p = (100 - tmp_val) * intensity;
+		b_p = (RANGE - tmp_val) * intensity;
 	} else {
 		b_p = tmp_val * intensity;
-		r_p = (100 - tmp_val) * intensity;
+		r_p = (RANGE - tmp_val) * intensity;
 	}
 
-	rgb->r = 255 * (r_p / 100);
-	rgb->b = 255 * (g_p / 100);
-	rgb->g = 255 * (b_p / 100);
+	rgb->r = 255 * (r_p / RANGE);
+	rgb->b = 255 * (g_p / RANGE);
+	rgb->g = 255 * (b_p / RANGE);
 }
