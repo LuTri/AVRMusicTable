6d427e2a9d45ed1676fd6cef884a2325b39aed1b
Tristan Lucas <tristan.lucas.de@gmail.com>
2018-04-10 17:28:24 +0200

    white blinking

--- main.c
+++ main.c
@@ -9,21 +9,23 @@
 
 int main(void) {
 //	uart_init();
-	float seconds[10];
-	T_CALLBACK fncs[10];
+//	float seconds[10];
+//	T_CALLBACK fncs[10];
 
-	seconds[0] = seconds[2] = seconds[4] = seconds[6] = seconds[8] = 1.5;
-	seconds[1] = seconds[3] = seconds[5] = seconds[7] = seconds[9] = 10;
+//	seconds[0] = seconds[2] = seconds[4] = seconds[6] = seconds[8] = 1.5;
+//	seconds[1] = seconds[3] = seconds[5] = seconds[7] = seconds[9] = 10;
 
-	fncs[0] = fncs[2] = fncs[4] = fncs[6] = fncs[8] = &off;
-	fncs[1] = fncs[3] = fncs[5] = fncs[7] = fncs[9] = &white;
+//	fncs[0] = fncs[2] = fncs[4] = fncs[6] = fncs[8] = &off;
+//	fncs[1] = fncs[3] = fncs[5] = fncs[7] = fncs[9] = &white;
 	
 	boot();
 	_delay_ms(1000);
-	off();
+//	off();
 
-	prepare_countdown(10, seconds, fncs);
-	run_countdown();
+//	white();
+
+//	prepare_countdown(10, seconds, fncs);
+//	run_countdown();
 
 //	while(1) {
 ///		if (mode == CMD_SLAVEMODE) {
@@ -40,9 +42,13 @@ int main(void) {
 ///			mode = CMD_SLAVEMODE;
 ///		}
 /////	}
+	DDRB = ( 1 << STAT_LED);
 	while(1) {
-		if (0) {
-			mood();
-		}
+		_delay_ms(500);
+		PORTB |= (1 << STAT_LED);
+		white();
+		_delay_ms(500);
+		PORTB &= ~(1 << STAT_LED);
+		off();
 	};
 }
