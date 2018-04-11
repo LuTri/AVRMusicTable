dea77f42214736b77b04f0729d4a15e93a2455c0
Tristan Lucas <tristan@adblockplus.org>
2018-04-11 11:50:55 +0200

    test / clean up main.c

--- main.c
+++ main.c
@@ -8,47 +8,9 @@
 #include <util/delay.h>
 
 int main(void) {
-//	uart_init();
-//	float seconds[10];
-//	T_CALLBACK fncs[10];
-
-//	seconds[0] = seconds[2] = seconds[4] = seconds[6] = seconds[8] = 1.5;
-//	seconds[1] = seconds[3] = seconds[5] = seconds[7] = seconds[9] = 10;
-
-//	fncs[0] = fncs[2] = fncs[4] = fncs[6] = fncs[8] = &off;
-//	fncs[1] = fncs[3] = fncs[5] = fncs[7] = fncs[9] = &white;
-	
 	boot();
 	_delay_ms(1000);
-//	off();
-
-//	white();
-
-//	prepare_countdown(10, seconds, fncs);
-//	run_countdown();
-
-//	while(1) {
-///		if (mode == CMD_SLAVEMODE) {
-///			slave();
-///		} else if (mode == CMD_MOOD) {
-///			mood();
-///		} else if (mode == CMD_SOUNDTOLIGHT) {
-///			sound();
-///		} else if (mode == CMD_WHITE) {
-///			white();
-///		} else if (mode == CMD_OFF) {
-///			off();
-///		} else {
-///			mode = CMD_SLAVEMODE;
-///		}
-/////	}
-	DDRB = ( 1 << STAT_LED);
 	while(1) {
-		_delay_ms(500);
-		PORTB |= (1 << STAT_LED);
-		white();
-		_delay_ms(500);
-		PORTB &= ~(1 << STAT_LED);
-		off();
+        off();
 	};
 }
