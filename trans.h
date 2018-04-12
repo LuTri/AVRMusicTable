28d8ced0238aee9b939c0d7c64caabd50a80851b
Tristan Lucas <tristan@adblockplus.org>
2018-04-12 11:37:02 +0200

    Implementing position translation

---
+++ trans.h
@@ -0,0 +1,12 @@
+#ifndef _TRANS_H
+#define _TRANS_H
+
+uint8_t coord_to_snakish(uint8_t x, uint8_t y);
+void snakish_to_coord(uint8_t snake, uint8_t* x, uint8_t* y);
+
+float distance_coord(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2);
+float distance_snake(uint8_t pos1, uint8_t pos2);
+
+#define MAX_DISTANCE 16.124515f
+
+#endif
