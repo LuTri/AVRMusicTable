64a00dfbf29a6d2ec606d38f1e9bdc01898f924e
Tristan Lucas <tristan@adblockplus.org>
2018-04-12 11:39:06 +0200

    Adding moods and modes

---
+++ modes.c
@@ -0,0 +1,98 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "modes.h"
+#include "color.h"
+#include "ws2812.h"
+#include "moods.h"
+
+extern cRGB leds[N_PACKS];
+
+uint8_t mode = CMD_WHITE;
+
+void slave(void) {
+	uint16_t bench;
+	char buf[2];
+
+	if (uart_available()) {
+		read_uart((uint8_t*)&leds);
+		bench = ws2812_setleds();
+		buf[0] = bench >> 8;
+		buf[1] = bench;
+		uart_putc(buf[0]);
+		uart_putc(buf[1]);
+	}
+}
+
+void rgb(uint8_t r, uint8_t g, uint8_t b) {
+	uint8_t idx;
+	for (idx = 0; idx < N_PACKS; idx++) {
+		leds[idx].r = r;
+        leds[idx].g = g;
+        leds[idx].b = b;;
+	}
+	ws2812_setleds();
+}
+
+void mood(void) {
+	static uint8_t func_idx = 0;
+
+	if (uart_available()) {
+		func_idx = uart_getc();
+	}
+
+	if ( fill_mood(0,100.0,1.0,func_idx) ) {
+		ws2812_setleds();
+	}
+}
+
+void sound(void) {
+}
+
+void white(void) {
+    rgb(255,255,255);
+}
+
+void red(void) {
+    rgb(255,0,0);
+}
+
+void blue(void) {
+    rgb(0,0,255);
+}
+
+void green(void) {
+    rgb(0,255,0);
+}
+
+void off(void) {
+    rgb(0,0,0);
+}
+
+
+void byte(uint16_t value) {
+	uint8_t idx;
+	for (idx = 0; idx < 16; idx++) {
+		leds[idx].r = leds[idx].g = leds[idx].b = 255 * (1 & (value >> idx));
+	}
+	ws2812_setleds();
+}
+
+void byte_offset(uint8_t value, uint8_t offset) {
+	uint8_t idx;
+	if (offset + 8 < N_PACKS) {
+		for (idx = 0; idx < 8; idx++) {
+			leds[idx + offset].r = leds[idx + offset].g = leds[idx + offset].b = 255 * (1 & (value >> idx));
+		}
+		ws2812_setleds();
+	}
+}
+
+void boot(void) {
+	uint8_t idx;
+	for (idx = 0; idx < N_PACKS; idx++) {
+		leds[idx].r = 255 * (1 & (idx));
+		leds[idx].g = 255 * (1 & (idx >> 1));
+		leds[idx].b = 255 * (1 & (idx >> 2));
+	}
+	ws2812_setleds();
+}
---
+++ modes.h
@@ -0,0 +1,22 @@
+#ifndef _MODES_H
+#define _MODES_H
+
+#include "globals.h"
+
+void slave(void);
+void mood(void);
+void sound(void);
+void white(void);
+void red(void);
+void green(void);
+void blue(void);
+void off(void);
+void byte(uint16_t value);
+
+void boot(void);
+
+void byte_offset(uint8_t value, uint8_t offset);
+
+extern uint8_t mode;
+
+#endif
